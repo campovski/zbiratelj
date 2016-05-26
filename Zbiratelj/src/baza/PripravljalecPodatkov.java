42e1d68359755a103ab32342d64d24de9f78414e
SlivnikN <nina.slivnik@gmail.com>
2016-05-26 23:54:29 +0200

    Prestavi branje gumbov v NovElementWindow in GlavnoOkno, dodaj dodajElemente

--- Zbiratelj/src/baza/PripravljalecPodatkov.java
+++ Zbiratelj/src/baza/PripravljalecPodatkov.java
@@ -5,8 +5,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import javax.swing.JCheckBox;
-
 import gui.GlavnoOkno;
 
 /**
@@ -19,7 +17,7 @@ import gui.GlavnoOkno;
 public class PripravljalecPodatkov {
 	
 	private static Map<String, List<List<String>>> slovar;
-	private static ArrayList<String> seznamZbirkZaIzbris;
+	private static Map<String, List<List<String>>> elementiZaDodajanje = new HashMap<String, List<List<String>>>();
 
 	/**
 	 * Naredi testni slovar. Mogoce v prihodnosti se kaj drugega...
@@ -85,19 +83,31 @@ public class PripravljalecPodatkov {
 	 * Metoda se pozene v novem vlaknu, ki po branju JCheckBox-ov pocaka, ali
 	 * bo uporabnik potrdil izbris. Ce ga, nadaljujemo, drugace se vlakno ustavi.
 	 */
-	public static void izbrisiZbirke(){
-		//TODO po generiranju seznama pocakaj, kaj bo naredilo GlavnoOkno
-		seznamZbirkZaIzbris = new ArrayList<String>();
-		Map<JCheckBox, String> slovarCheckGumbov = GlavnoOkno.getSlovarCheckBoxZbirka();
-		for (JCheckBox kljuc : slovarCheckGumbov.keySet()){
-			if (kljuc.isSelected()){
-				seznamZbirkZaIzbris.add(slovarCheckGumbov.get(kljuc));
+	public static void izbrisiZbirke(List<String> zbirke){
+		SqlManager.odstraniZbirke(zbirke);
+		System.out.println(zbirke);
+	}
+	
+	public static void dodajElement(String zbirka, List<String> element){
+		if (elementiZaDodajanje.containsKey(zbirka)){
+			if (!elementiZaDodajanje.get(zbirka).contains(element)){
+				elementiZaDodajanje.get(zbirka).add(element);
 			}
 		}
-		
-		// wait...
-		SqlManager.odstraniZbirke(seznamZbirkZaIzbris);
-		System.out.println(seznamZbirkZaIzbris);
+		else {
+			List<List<String>> dodanElement = new ArrayList<List<String>>();
+			dodanElement.add(element);
+			elementiZaDodajanje.put(zbirka, dodanElement);
+		}
+		System.out.println(elementiZaDodajanje);
+	}
+	
+	public static void dodajElemente(){
+		if (!elementiZaDodajanje.isEmpty()){
+			SqlManager.dodajElemente(elementiZaDodajanje);
+			System.out.println("To so dodani elementi: " + elementiZaDodajanje);
+			elementiZaDodajanje.clear();
+		}
 	}
 	
 	/**
@@ -105,16 +115,9 @@ public class PripravljalecPodatkov {
 	 * niso tako nevarni za izbris, kakor zbirke, tu ni potrebno preverjati, ali res zelimo
 	 * izbris ali ne.
 	 */
-	public static void izbrisiElemente(){
-		List<List<String>> seznamElementovZaIzbris = new ArrayList<List<String>>();
-		Map<JCheckBox, List<String>> slovarCheckBoxElement = GlavnoOkno.getSlovarCheckBoxElement();
-		for (JCheckBox kljuc : slovarCheckBoxElement.keySet()){
-			if (kljuc.isSelected()){
-				seznamElementovZaIzbris.add(slovarCheckBoxElement.get(kljuc));
-			}
-		}
-		SqlManager.odstraniElemente(GlavnoOkno.getZbirkaZaRisanje(), seznamElementovZaIzbris);
-		System.out.println(seznamElementovZaIzbris);
+	public static void izbrisiElemente(List<List<String>> elementi){
+		SqlManager.odstraniElemente(GlavnoOkno.getZbirkaZaRisanje(), elementi);
+		System.out.println(elementi);
 	}
 
 	/**
@@ -122,12 +125,5 @@ public class PripravljalecPodatkov {
 	 */
 	public Map<String, List<List<String>>> getSlovar() {
 		return slovar;
-	}
-	
-	/**
-	 * @return seznamZbirkZaIzbris
-	 */
-	public static ArrayList<String> getSeznamZbirkZaIzbris() {
-		return seznamZbirkZaIzbris;
 	}
 }
